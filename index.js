4114dd6563f3ccfb26083329fa9d8b5a924b2e7c
Bibek Luitel <bibekluitel2011@gmail.com>
2019-01-04 00:13:43 +0530

    Adding LIb folder and Changing test style

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+const { set, get } = require('./lib')
+
 function JMON(data) {
   this.data  =  data
 }
 
+JMON.prototype.set = set;
+
+JMON.prototype.get = get
+
 JMON.prototype.print = function() {
-  console.log(this.data)
+  console.log(this)
 }
 
-module.exports = {
-  JMON
-}
+module.exports = JMON
--- test/test.js
+++ test/test.js
@@ -1,5 +1,12 @@
-const { JMON } = require('../index')
+var assert = require('assert');
 
-const d = new JMON({'test': 1});
+const JMON  = require('../index')
 
-d.print();
+describe('Testing Basic Functions', function () {
+    it('Prints Set Value', function () {
+        const d = new JMON({'test': 1});
+        const newObj = {'test': 2};
+        d.set(newObj);
+        assert.deepEqual(d.data, newObj);
+    })
+})
