c7092498cc502c60709482d0686f9d3efed5529a
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-01-12 00:25:36 +0530

    restructing of proto, changes tests for restructing

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var _ = require('lodash');
 
-var {
-  commit,
-  get,
-  isObject,
-  push,
-  set,
-} = require('./lib');
+var { isObject } = require('./lib');
 
 
 function JMON(data) {
@@ -32,13 +26,66 @@ function JMON(data) {
   });
 }
 
+
 JMON.prototype.isCreated = false;
 JMON.prototype.isUpdated = false;
 JMON.prototype.isDeleted = false;
-JMON.prototype.commit = commit;
-JMON.prototype.get = get;
-JMON.prototype.push = push;
-JMON.prototype.set = set;
+
+
+/**
+ * This will commit current changes and treat current data as initial data
+ * Also clears, createdList, deletedList, updatedList
+ */
+JMON.prototype.commit = function() {
+
+  this.initialData = _.cloneDeep(this.data);
+  Object.getPrototypeOf(this).isCreated = false;
+  Object.getPrototypeOf(this).isUpdated = false;
+  Object.getPrototypeOf(this).isDeleted = false;
+};
+
+
+/**
+ * @param {key/path to search in the object} key
+ * @param {if key/path is not found then this is used} defaultValue
+ * Returns value of input key from the currentData.
+ * Or returns defaultValue if key is not found
+ */
+JMON.prototype.get = function(key) {
+
+  var path = key.split('.');
+  var sourceData = this;
+  path.map(function(key) {
+    sourceData = _.get(sourceData.data, key);
+  });
+
+  return sourceData;
+};
+
+
+/**
+ * Check whether the key already exists or not.
+ * If key does not exists then throw error saying use push function
+ * else if value is not object set the value
+ * else set the value as new JMON object
+ * @param {*} key
+ * @param {*} value
+ */
+JMON.prototype.set = function(key, value) {
+
+  // Updating isUpdated flag.
+  Object.getPrototypeOf(this).isUpdated = true;
+
+  // If value is not JSON
+  if (!isObject(value)) {
+    _.set(this.data, key, value);
+    return true;
+  }
+
+  // If value is JSON
+  _.set(this.data, key, new JMON(value));
+  return true;
+};
 
 
 module.exports = JMON;
--- test/test.js
+++ test/test.js
@@ -15,18 +15,12 @@ var SAMPLE_JSON_1 = {
   'url': 'https://github.com/bibekluitel/JMON',
 };
 
-// var SAMPLE_JSON_3 = {
-//   'JMON': {
-//     'name': {
-//       'people': 'bibek',
-//     },
-//     'author': 'Bibek',
-//     'url': 'https://github.com/bibekluitel/JMON',
-//   },
-// };
+var TEST_JSON = {
+  'people': 'Arshad',
+};
 
-// var SAMPLE_JSON_STIRNGIFIED = '{"JMON":{"name":"JMON","author":"Bibek",'
-// + '"url":"https://github.com/bibekluitel/JMON"}}';
+// var SAMPLE_JSON_STIRNGIFIED = '{'JMON':{'name':'JMON','author':'Bibek','
+// + ''url':'https://github.com/bibekluitel/JMON'}}';
 
 describe('VCOnstructor Test', function() {
   it('Compare Constructor Object', function() {
@@ -34,7 +28,6 @@ describe('VCOnstructor Test', function() {
     expect(typeof (jmon.set)).to.be.equal('function');
     expect(typeof (jmon.get)).to.be.equal('function');
     expect(typeof (jmon.commit)).to.be.equal('function');
-    expect(typeof (jmon.push)).to.be.equal('function');
     expect(jmon.isUpdated).to.be.false;
     expect(jmon.isCreated).to.be.false;
     expect(jmon.isDeleted).to.be.false;
@@ -50,61 +43,37 @@ describe('verifies the set and get operations', function() {
     expect(result).to.true;
     expect(jmon.isUpdated).to.true;
     expect(jmon.get('JMON').data).to.deep.equal(SAMPLE_JSON_1);
-
-    result = jmon.set('JMON.no_author', 'arshad');
-    expect(result).to.false;
   });
 
-  // it('validates set object value', function() {
-  //   var jmon = new JMON(SAMPLE_JSON);
-  //   expect(jmon.get('JMON').data).to.deep.equal(SAMPLE_JSON.JMON);
+  it('validates set object value', function() {
+    var jmon = new JMON(SAMPLE_JSON);
+    expect(jmon.get('JMON').data).to.deep.equal(SAMPLE_JSON.JMON);
 
-  //   var result = jmon.get('JMON').set('name', { 'people': 'Arshad'});
-  //   expect(result).to.true;
-  //   expect(jmon.isUpdated).to.true;
-  //   console.log(result);
-  // expect(jmon.get('JMON').data).to.deep.equal(SAMPLE_JSON_1);
+    var result = jmon.get('JMON').set('name', { 'people': 'Arshad'});
+    expect(result).to.true;
+    expect(jmon.isUpdated).to.true;
+    expect(jmon.get('JMON.name').data).to.deep.equal(TEST_JSON);
+  });
 
-  // result = jmon.set('JMON.no_author', 'arshad');
-  // expect(result).to.false;
-  // });
+  it('validates set new object value', function() {
+    var jmon = new JMON(SAMPLE_JSON);
+    expect(jmon.get('JMON').data).to.deep.equal(SAMPLE_JSON.JMON);
+
+    var result = jmon.get('JMON').set('new_name', { 'people': 'Arshad'});
+    expect(result).to.true;
+    expect(jmon.isUpdated).to.true;
+    expect(jmon.get('JMON.new_name').data).to.deep.equal(TEST_JSON);
+    expect(jmon.get('JMON.name')).to.deep.equal('JMON');
+  });
 });
 
-// describe('Verifies commit functionality', function(){
-//   it('Commits the current changes', function() {
-//     var jmon = new JMON(SAMPLE_JSON);
-//     var result = jmon.set('JMON.author', 'arshad');
-//     expect(result).to.true;
-//     expect(jmon.initialData).to.deep.equal(SAMPLE_JSON);
-//     jmon.commit();
-//     expect(jmon.initialData).to.deep.equal(SAMPLE_JSON_3);
-//   });
-// });
-
-// // Add test related set/get operations
-// describe('Verifies the set and get operations', function() {
-//   it('Prints Set Value', function() {
-//     var jmon = new JMON(SAMPLE_JSON);
-//     jmon.set('JMON.name.people', 'bibek');
-//     expect(jmon.data).to.deep.equal(SAMPLE_JSON_3);
-//   });
-// });
-
-// describe('Verifies commit functionality', function(){
-//   it('Commits the current changes', function() {
-//     var jmon = new JMON(SAMPLE_JSON);
-//     jmon.set('JMON.name.people', 'bibek');
-//     jmon.commit();
-//     expect(jmon.initialData).to.deep.equal(SAMPLE_JSON_3);
-//   });
-// });
-
-// Add test related to printing and formatting operations
-// describe('Verify printing related operations', () => {
-//   it('should match the returned stringified response with defined stringified value', () => {
-//     var jmon = new JMON(SAMPLE_JSON);
-//     var response = jmon.stringify();
-//     expect(response).to.be.a('string');
-//     expect(response).to.equal(SAMPLE_JSON_STIRNGIFIED);
-//   });
-// });
+describe('Verifies commit functionality', function(){
+  it('Commits the current changes', function() {
+    var jmon = new JMON(SAMPLE_JSON);
+    var result = jmon.set('JMON.people', 'Arshad');
+    expect(result).to.true;
+    expect(jmon.initialData).to.deep.equal(SAMPLE_JSON);
+    jmon.commit();
+    expect(jmon.initialData.JMON.people).to.deep.equal(TEST_JSON.people);
+  });
+});
