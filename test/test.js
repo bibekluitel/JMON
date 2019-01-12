fe38ce77f00ac9604a8d7014a6897d572a5b70b4
Bibek Luitel <bibekluitel2011@gmail.com>
2019-01-12 23:27:49 +0530

    Set value/ Check for DirtyCHild  Implementation

--- test/test.js
+++ test/test.js
@@ -19,9 +19,6 @@ var TEST_JSON = {
   'people': 'Arshad',
 };
 
-// var SAMPLE_JSON_STIRNGIFIED = '{'JMON':{'name':'JMON','author':'Bibek','
-// + ''url':'https://github.com/bibekluitel/JMON'}}';
-
 describe('VCOnstructor Test', function() {
   it('Compare Constructor Object', function() {
     var jmon = new JMON(SAMPLE_JSON);
@@ -35,45 +32,82 @@ describe('VCOnstructor Test', function() {
 });
 
 describe('verifies the set and get operations', function() {
+
   it('validates set string value', function() {
     var jmon = new JMON(SAMPLE_JSON);
     expect(jmon.get('JMON').data).to.deep.equal(SAMPLE_JSON.JMON);
 
     var result = jmon.get('JMON').set('author', 'Arshad');
     expect(result).to.true;
-    expect(jmon.isUpdated).to.true;
+
+    expect(jmon.isUpdated).to.false;
+
+    expect(jmon.get('JMON').isUpdated).to.true;
+
+    expect(jmon.checkIfUpdated()).to.true;
+
     expect(jmon.get('JMON').data).to.deep.equal(SAMPLE_JSON_1);
   });
 
   it('validates set object value', function() {
+
     var jmon = new JMON(SAMPLE_JSON);
+
     expect(jmon.get('JMON').data).to.deep.equal(SAMPLE_JSON.JMON);
 
     var result = jmon.get('JMON').set('name', { 'people': 'Arshad'});
+
+
     expect(result).to.true;
-    expect(jmon.isUpdated).to.true;
+
+    expect(jmon.isUpdated).to.false;
+
+    expect(jmon.get('JMON').isUpdated).to.true;
+
+    expect(jmon.get('JMON.name').isCreated).to.true;
+
     expect(jmon.get('JMON.name').data).to.deep.equal(TEST_JSON);
+
+
   });
 
   it('validates set new object value', function() {
+
     var jmon = new JMON(SAMPLE_JSON);
+
     expect(jmon.get('JMON').data).to.deep.equal(SAMPLE_JSON.JMON);
 
-    var result = jmon.get('JMON').set('new_name', { 'people': 'Arshad'});
+    var result = jmon.get('JMON').set('new_name', TEST_JSON);
+
     expect(result).to.true;
-    expect(jmon.isUpdated).to.true;
+
+    expect(jmon.isUpdated).to.false;
+
+    expect(jmon.get('JMON').isUpdated).to.true;
+
+    expect(jmon.get('JMON.new_name').isCreated).to.true;
+
     expect(jmon.get('JMON.new_name').data).to.deep.equal(TEST_JSON);
+
     expect(jmon.get('JMON.name')).to.deep.equal('JMON');
   });
 });
 
+
 describe('Verifies commit functionality', function(){
   it('Commits the current changes', function() {
+
     var jmon = new JMON(SAMPLE_JSON);
+
     var result = jmon.set('JMON.people', 'Arshad');
+
     expect(result).to.true;
+
     expect(jmon.initialData).to.deep.equal(SAMPLE_JSON);
+
     jmon.commit();
+
     expect(jmon.initialData.JMON.people).to.deep.equal(TEST_JSON.people);
+
   });
 });
